c3e20e911ce1af5c0f9a8814006aae29c5c02305
carl-larson <clarson08@gmail.com>
2020-09-25 15:03:31 -0500

    continuing Luis Zuno's tutorial

--- practice1/game.js
+++ practice1/game.js
@@ -2,10 +2,16 @@
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 256,
+    height: 272,
     backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
+    scene: [Scene1, Scene2],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    }
 };
 
 
